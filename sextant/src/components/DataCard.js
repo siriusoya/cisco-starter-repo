1f14893ceee3ae1631bc81c4fd97a2e916b65e9b
siriusoya <siriusoya@gmail.com>
2023-08-14 20:45:09 +0700

    created content component

---
+++ sextant/src/components/DataCard.js
@@ -0,0 +1,13 @@
+function DataCard() {
+    return (
+      <>
+        <div className="dataContainer">
+          <h3 className="dataTitle">Your public IPv6 address is:</h3>
+          <hr className="dataUL" />
+          <h2 className="data">25.743.235.754</h2>
+        </div>
+      </>
+    );
+  }
+  
+  export default DataCard;
