fbe35ae03db465cdd288b3ff960dc108259ff848
siriusoya <siriusoya@gmail.com>
2023-08-14 22:53:18 +0700

    connected to ipify

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -1,14 +1,38 @@
+import { useState } from 'react';
+import { useEffect } from 'react';
 import DataCard from "./components/DataCard";
 
 function App() {
+  const [IPv4, setIPv4] = useState();
+  const [IPv6, setIPv6] = useState();
+
+
+  async function getIPv4() {
+    const response = await fetch("https://api.ipify.org?format=json");
+    const data = await response.json();
+    setIPv4(data.ip)
+  }
+
+  async function getIPv6() {
+    const response = await fetch("http://api64.ipify.org?format=json");
+    const data = await response.json();
+    setIPv6(data.ip)
+    console.log(data.ip)
+  }
+
+  useEffect(() => {
+    getIPv4()
+    getIPv6()
+  }, []);
+
   return (
     <>
       <div className="banner">
         <h1 className="title">Sextant</h1>
       </div>
       <div className="contentContainer">
-        <DataCard />
-        <DataCard />
+        <DataCard IPv4={IPv4} />
+        <DataCard IPv6={IPv6} />
       </div>
     </>
   );
--- sextant/src/components/DataCard.js
+++ sextant/src/components/DataCard.js
@@ -1,13 +1,20 @@
-function DataCard() {
-    return (
-      <>
-        <div className="dataContainer">
-          <h3 className="dataTitle">Your public IPv6 address is:</h3>
-          <hr className="dataUL" />
-          <h2 className="data">25.743.235.754</h2>
-        </div>
-      </>
-    );
-  }
-  
-  export default DataCard;
+function DataCard(props) {
+  const { IPv4, IPv6 } = props;
+
+  return (
+    <>
+      {IPv4 && <div className="dataContainer">
+        <h3 className="dataTitle">Your public IPv4 address is:</h3>
+        <hr className="dataUL" />
+         <h2 className="data">{IPv4}</h2>
+      </div> }
+      {IPv6 && <div className="dataContainer">
+        <h3 className="dataTitle">Your public IPv6 address is:</h3>
+        <hr className="dataUL" />
+         <h2 className="data">{IPv6}</h2>
+      </div> }
+    </>
+  );
+}
+
+export default DataCard;
